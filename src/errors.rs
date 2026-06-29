9d656dbbe722faf61d13b8be927ce89eae011ad0
Reto Trappitsch <reto@galactic-forensics.space>
2026-06-29 16:12:12 +0200

    add range check errors

--- src/errors.rs
+++ src/errors.rs
@@ -3,7 +3,7 @@
 //! TODO: This needs to be tested with at least a "string" and a "&[u8]"/"bytes" instrument to make
 //! sure we have captured all possible error types (for now).
 
-use std::{fmt::Debug, io, num, string};
+use std::{ffi::os_str::Display, fmt::Debug, io, num, string};
 
 use thiserror::Error;
 
@@ -43,4 +43,51 @@ pub enum InstrumentRsError {
         "Could not convert the message received from the instrument {msg:?} into the specified type."
     )]
     BadInstrumentResponseVecU8 { msg: Vec<u8> },
+    /// A unitful value is out of range.
+    ///
+    /// This is generally used as an error to show the user that that a unitful value is out of range.
+    #[error(
+        "Provided value {val} {unit} is out of range:\n \
+            - Minimum value allowed: {val_min} {unit}\n \
+            - Maximum value allowed: {val_max} {unit}"
+    )]
+    UnitfulValueOutOfRange {
+        unit: String,
+        val: f64,
+        val_min: f64,
+        val_max: f64,
+    },
+    /// A float value is out of range.
+    #[error(
+        "Provided float value {val} is out of range:\n \
+            - Minimum value allowed: {val_min}\n \
+            - Maximum value allowed: {val_max}"
+    )]
+    FloatValueOutOfRange {
+        val: f64,
+        val_min: f64,
+        val_max: f64,
+    },
+    /// A signed integer value is out of range.
+    #[error(
+        "Provided integer value {val} is out of range:\n \
+            - Minimum value allowed: {val_min}\n \
+            - Maximum value allowed: {val_max}"
+    )]
+    IIntOutOfRange {
+        val: isize,
+        val_min: isize,
+        val_max: isize,
+    },
+    /// A unsigned integer value is out of range.
+    #[error(
+        "Provided integer value {val} is out of range:\n \
+            - Minimum value allowed: {val_min}\n \
+            - Maximum value allowed: {val_max}"
+    )]
+    UIntOutOfRange {
+        val: usize,
+        val_min: usize,
+        val_max: usize,
+    },
 }
