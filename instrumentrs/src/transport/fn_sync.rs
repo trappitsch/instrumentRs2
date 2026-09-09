84eab7cedfeac0e0f86427f109f001c15a8f9555
Reto Trappitsch <reto@galactic-forensics.space>
2026-09-09 16:32:31 +0200

    add error types, add function to read n bytes
    New error types are:
    
    - Error if a checksum mismatch happened.
    - Negative response error, e.g., when the command was fine but the
      instrument answered with in a negative way, e.g., command not
      recognized.

--- instrumentrs/src/transport/fn_sync.rs
+++ instrumentrs/src/transport/fn_sync.rs
@@ -21,7 +21,33 @@ pub fn write_all<I: Write>(
     Ok(())
 }
 
+/// Function to read a defined number of bytes and return the read as a Vec<u8>.
+///
+/// This function is helpful when working with instruments that, e.g., submit some header that
+/// contains the number of data bytes (which might be the bytes of interest) to read.
+/// Arguments:
+/// - interface: a &mut of the interface from which to read.
+/// - num_bytes: The number of bytes that we want to read.
+pub fn read_number_of_bytes<I: Read>(
+    interface: &mut I,
+    num_bytes: usize,
+) -> Result<Vec<u8>, InstrumentError> {
+    let mut ret: Vec<u8> = Vec::with_capacity(num_bytes);
+
+    let mut buf = [0u8];
+    for _ in 0..num_bytes {
+        interface.read_exact(&mut buf)?;
+        ret.push(buf[0]);
+    }
+
+    Ok(ret)
+}
+
 /// Function to read until a terminator is found and then returns a Vec<u8> of what was in it.
+///
+/// Arguments:
+/// - interface: a &mut of the interface from which to read.
+/// - terminator: the terminator we are looking for.
 pub fn read_until_terminator<I: Read>(
     interface: &mut I,
     terminator: &[u8],
